d2cafc9c13abb6a4645a232963316c9d0a9210b2
Lizbeth <lizstar3@outlook.es>
2025-11-16 17:28:23 -0600

    vista login agregada

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,9 +1,10 @@
 // routes/dashboardRoutes.js
 import express from "express";
-import { mostrarDashboard, reportes } from "../controllers/dashboardController.js";
+import { mostrarDashboard, reportes, login } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
+router.get("/login", login);
 router.get("/dashboard", mostrarDashboard);
 router.get("/", (req, res) => res.redirect("/dashboard"));
 
